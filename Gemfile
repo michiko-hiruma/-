a92a0657d59606e925f4ec96f3c6dac544d77e22
hirumamichiko <hiruma7878@gmail.com>
2021-12-08 03:04:37 +0000

    g24

--- Gemfile
+++ Gemfile
@@ -4,8 +4,6 @@ gem 'rails',        '~> 5.1.6'
 gem 'bcrypt'
 gem 'faker'
 gem 'bootstrap-sass'
-gem 'will_paginate'
-gem 'bootstrap-will_paginate'
 gem 'puma',         '~> 3.7'
 gem 'sass-rails',   '~> 5.0'
 gem 'uglifier',     '>= 1.3.0'
@@ -14,7 +12,10 @@ gem 'jquery-rails'
 gem 'turbolinks',   '~> 5'
 gem 'jbuilder',     '~> 2.5'
 gem 'rails-i18n'
-gem 'rinku'
+gem 'active_decorator'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
+gem 'rails_autolink'
 
 group :development, :test do
   gem 'sqlite3'
@@ -26,6 +27,10 @@ group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :production do
+  gem 'pg', '0.20.0'
 end
 
 # Windows環境ではtzinfo-dataというgemを含める必要があります
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,54 +3,49 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i(show edit update destroy)
   before_action :logged_in_user
   before_action :correct_user
+  
+  def index
+    @tasks = @user.tasks
+  end
 
-    def index
-        @tasks = current_user.tasks.all.sort.reverse
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = @user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました。"
+      redirect_to user_tasks_url @user
+    else
+      render :new
     end
+  end
+
+  def edit
     
-    def new
-        @task = Task.new
+  end
+  
+  def update
+    if @task.update_attributes(task_params)
+      flash[:success] = "タスクを更新しました。"
+      redirect_to user_task_url(@user, @task)
+    else
+      render :edit
     end
-    
-    def create
-        
-        @task = current_user.tasks.new(task_params)
-        
-        if @task.save
-            flash[:notice] = "保存出来ました。"
-            redirect_to tasks_url(id: current_user.id)
-        else
-            flash.now[:danger] ="保存出来ませんでした。"
-            render :new
-            
-        end
-    end
-    def edit
-        @task = Task.find(params[:id])
-    end
-        
-    def update
-        @task = Task.find(params[:id])
-        if @task.update(task_params)
-            redirect_to task_url
-        else
-            render :edit
-        end
-    end
-    
-    def show
-        @task = Task.find(params[:id])
-    end
-    
-    def destroy
-      @task = Task.find(params[:id])
-      @task.destroy
-      flash[:success] = "#{@task.name}のデータを削除しました。"
-      redirect_to tasks_url
-    end
-    
-    private
-    
+  end
+  
+  def destroy
+    @task.destroy
+    flash[:success] = "タスクを削除しました。"
+    redirect_to user_tasks_url @user
+  end
+  
+  private
+
     def task_params
       params.require(:task).permit(:name, :description)
     end
@@ -66,4 +61,3 @@ class TasksController < ApplicationController
       end
     end
 end
- 
